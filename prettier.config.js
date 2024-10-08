b3d0a868da13ed75dbc0ce02b3d05d44bf4619b5
Ilya Prymak <primak2506@gmail.com>
2024-10-08 15:37:08 +0300

    fix: config files extension

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import("prettier").Config}
+ */
+
+export default {
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: 'crlf'
+}
